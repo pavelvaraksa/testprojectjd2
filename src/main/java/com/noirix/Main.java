34113d00593a757b141467a748640f75b1cb85e1
Viachaslau Kalevich <cto@noirix.com>
2020-09-15 21:22:37 +0300

    Cherry pick test

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!1111");
+        System.out.println("String for cherry pick");
     }
 }
