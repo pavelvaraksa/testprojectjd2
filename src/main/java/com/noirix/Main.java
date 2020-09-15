0fba27c86a546155287afd231c6599c05d2f7b8a
Viachaslau Kalevich <cto@noirix.com>
2020-09-15 21:19:40 +0300

    Hot keys tips

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -2,6 +2,11 @@ package com.noirix;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Ctrl+W - word checking
+        //Alt+Shift - multiple cursor
+        //Alt + Mouse movements - multiple rows checking
+
         System.out.println("Hello, world!");
         System.out.println("Hello, world!1111");
         System.out.println("Hello, world!1111111");
