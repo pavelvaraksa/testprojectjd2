cde1abd23136097b8585b6c4cf459fb549fac85e
Viachaslau Kalevich <cto@noirix.com>
2020-09-24 21:49:19 +0300

    Add Servlet logic

--- src/main/java/com/noirix/controller/FrontController.java
+++ src/main/java/com/noirix/controller/FrontController.java
@@ -1,4 +1,32 @@
 package com.noirix.controller;
 
-public class FrontController {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FrontController extends HttpServlet {
+    public FrontController() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doRequest(req, resp);
+    }
+
+    private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/bye");
+        if (dispatcher != null) {
+            System.out.println("Forward will be done!");
+            dispatcher.forward(req, resp);
+        }
+    }
 }
