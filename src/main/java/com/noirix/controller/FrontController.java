8c62714cc0c2e0fa422341ee3a4706e637f7141a
Viachaslau Kalevich <cto@noirix.com>
2020-10-08 21:09:14 +0300

    JDBC connection + web user list

--- src/main/java/com/noirix/controller/FrontController.java
+++ src/main/java/com/noirix/controller/FrontController.java
@@ -1,13 +1,21 @@
 package com.noirix.controller;
 
+import com.noirix.domain.User;
+import com.noirix.repository.UserRepository;
+import com.noirix.repository.impl.UserRepositoryImpl;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class FrontController extends HttpServlet {
+
+    public static final UserRepository userRepository = new UserRepositoryImpl();
+
     public FrontController() {
         super();
     }
@@ -23,9 +31,12 @@ public class FrontController extends HttpServlet {
     }
 
     private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/bye");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/hello");
         if (dispatcher != null) {
             System.out.println("Forward will be done!");
+
+            req.setAttribute("userName", userRepository.findAll().stream().map(User::getName).collect(Collectors.joining(",")));
+
             dispatcher.forward(req, resp);
         }
     }
--- src/main/java/com/noirix/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/noirix/repository/impl/UserRepositoryImpl.java
@@ -1,22 +1,39 @@
 package com.noirix.repository.impl;
 
+import com.noirix.domain.Gender;
 import com.noirix.domain.User;
 import com.noirix.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepositoryImpl implements UserRepository {
 
+    public static final String POSTRGES_DRIVER_NAME = "org.postgresql.Driver";
     public static final String DATABASE_URL = "jdbc:postgresql://localhost:";
     public static final int DATABASE_PORT = 5432;
     public static final String DATABASE_NAME = "/webinar_database";
     public static final String DATABASE_LOGIN = "postgres";
-    public static final String DATABASE_PASSWORD = "postgres";
+    public static final String DATABASE_PASSWORD = "root";
+
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String SURNAME = "surname";
+    private static final String BIRTH_DATE = "birth_date";
+    private static final String GENDER = "gender";
+    private static final String CREATED = "created";
+    private static final String CHANGED = "changed";
+    private static final String WEIGHT = "weight";
 
     @Override
     public List<User> search(String query) {
-
         return null;
     }
 
@@ -27,7 +44,47 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> findAll() {
-        return null;
+        final String findAllQuery = "select * from m_users order by id";
+
+        List<User> result = new ArrayList<>();
+
+        Connection connection;
+        Statement statement;
+        ResultSet rs;
+
+        try {
+            Class.forName(POSTRGES_DRIVER_NAME);
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBC Driver Cannot be loaded!");
+            throw new RuntimeException("JDBC Driver Cannot be loaded!");
+        }
+
+        String jdbcURL = StringUtils.join(DATABASE_URL, DATABASE_PORT, DATABASE_NAME);
+
+        try {
+            connection = DriverManager.getConnection(jdbcURL, DATABASE_LOGIN, DATABASE_PASSWORD);
+            statement = connection.createStatement();
+            rs = statement.executeQuery(findAllQuery);
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong(ID));
+                user.setName(rs.getString(NAME));
+                user.setSurname(rs.getString(SURNAME));
+                user.setBirthDate(rs.getDate(BIRTH_DATE));
+                user.setGender(Gender.valueOf(rs.getString(GENDER)));
+                user.setCreated(rs.getTimestamp(CREATED));
+                user.setChanged(rs.getTimestamp(CHANGED));
+                user.setWeight(rs.getFloat(WEIGHT));
+
+                result.add(user);
+            }
+
+            return result;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException("SQL Issues!");
+        }
     }
 
     @Override
